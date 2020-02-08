a22cdaaad17ad3817be81b29d1ea52c65d19048c
alex <batmandocode@gmail.com>
2020-02-08 15:44:13 +1100

    1. 添加页面

---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NavHeaderVue></NavHeaderVue>
+            <router-view></router-view>
+        <NavFooterVue></NavFooterVue>
+        
+    </div>
+</template>
+
+<script>
+import NavHeaderVue from '../components/NavHeader.vue'
+import NavFooterVue from '../components/NavFooter.vue'
+
+export default {
+    
+}
+</script>
