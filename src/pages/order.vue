5e37e1f67c7cc0c212f63e99f5a05f237d3ab078
alex <batmandocode@gmail.com>
2020-02-15 12:30:55 +1100

    footer 吸底的处理

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
-      order page
-    <order-header></order-header>
+  <div id="order">
+    <order-header></order-header>order page
+    <div id="content"></div>
     <router-view></router-view>
-    <order-footer></order-footer>
+
+    <div id="footer">
+      <order-footer></order-footer>
+    </div>
   </div>
 </template>
 
 <script>
-import OrderHeader from '../components/OrderHeader.vue';
-import OrderFooter from '../components/OrderFooter.vue';
+import OrderHeader from "../components/OrderHeader.vue";
+import OrderFooter from "../components/NavFooter";
 
 export default {
-    components: {OrderHeader, OrderFooter}
+  components: { OrderHeader, OrderFooter }
 };
 </script>
 
-<style>
-</style>
+<style lang="scss">
+#order {
+  position: relative;
+  min-height: 100vh;
+  #content{
+    height: 1000px;
+  }
+  #footer{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+}
+</style>>
